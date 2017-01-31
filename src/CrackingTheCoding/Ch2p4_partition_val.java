885337ead27bab040e383f25e4ce19e8bae53df4
Shreyash <shreyasdsharma@gmail.com>
2017-01-31 00:33:16 -0700

    Ch2p4

--- src/CrackingTheCoding/Ch2p4_partition_val.java
+++ src/CrackingTheCoding/Ch2p4_partition_val.java
@@ -7,31 +7,39 @@ import CrackingTheCoding.LinkedListNode;
 public class Ch2p4_partition_val {
 
 	public static void main(String[] args){
-	LinkedListNode beforeStart = null;
-	LinkedListNode afterStart = null;
 	
 	
-	
+		
 	
 	}
 	
 	//Pass the head of linkedlist & value of partition around
 	public LinkedListNode partition(LinkedListNode node, int x){
+		LinkedListNode beforeStart = null;
+		LinkedListNode afterStart = null;
 		
 		//Partition list
 		while(node != null){
 			LinkedListNode next = node.next;
-			if(next.value < x ){
+			if(node.value < x ){
 				node.next = beforeStart;
 				beforeStart = node;
-				
 			}else{
-				
+				node.next = afterStart;
+				afterStart = node;
 			}
-			
-			
+		node = next;	
 		}
-		return null;
+		
+		//merge both the lists
+		if(beforeStart == null) return afterStart;
+		LinkedListNode head = beforeStart;
+		while(beforeStart.next != null){
+			beforeStart = beforeStart.next;
+		}
+		beforeStart.next = afterStart; 
+		
+		return head;
 	}
 	
 	
