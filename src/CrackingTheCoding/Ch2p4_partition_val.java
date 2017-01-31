92de455a4d8322b30e2a05d1f93a0b4ae8e3da76
Shreyash <shreyasdsharma@gmail.com>
2017-01-31 00:08:08 -0700

    Q 2.4

--- src/CrackingTheCoding/Ch2p4_partition_val.java
+++ src/CrackingTheCoding/Ch2p4_partition_val.java
@@ -1,9 +1,37 @@
 package CrackingTheCoding;
+import CrackingTheCoding.LinkedListNode;
+
+//partition linkedlist around a vaalue x,
+//   such that all nodes <x come before all node >=x
 
 public class Ch2p4_partition_val {
 
 	public static void main(String[] args){
+	LinkedListNode beforeStart = null;
+	LinkedListNode afterStart = null;
+	
+	
+	
+	
+	}
+	
+	//Pass the head of linkedlist & value of partition around
+	public LinkedListNode partition(LinkedListNode node, int x){
 		
+		//Partition list
+		while(node != null){
+			LinkedListNode next = node.next;
+			if(next.value < x ){
+				node.next = beforeStart;
+				beforeStart = node;
+				
+			}else{
+				
+			}
+			
+			
+		}
+		return null;
 	}
 	
 	
